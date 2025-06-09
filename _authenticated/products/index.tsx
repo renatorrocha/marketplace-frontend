df8a4320c4c2ea06dd8c47bb43f7c36dceb0afd0
renatorrocha <renatorrodrigues2002@gmail.com>
2025-06-09 08:49:10 -0300

    refactor: remove _authenticated from root

--- _authenticated/products/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_authenticated/products/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_authenticated/products/"!</div>
-}
